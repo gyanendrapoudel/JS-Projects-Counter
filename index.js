c80c991900a593a90494f5c9cbe3b325e653eec5
Gyanendra <gyanen56@gmail.com>
2024-04-12 16:37:40 +1000

    project completed

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const increase = document.querySelector('.increase')
+const decrease = document.querySelector('.decrease')
+const reset = document.querySelector('.reset')
+const number = document.querySelector('.number')
+
+let value = parseInt(number.textContent);
+
+increase.addEventListener(('click'),()=>{
+    console.log(value)
+    if(value>0){
+        number.classList.remove('red')
+    }
+    value=+value+1;
+    number.textContent=value
+})
+
+decrease.addEventListener('click', () => {
+  value = +value - 1
+  if(value<0){
+    number.classList.add('red');
+  }
+  number.textContent = value
+
+})
+reset.addEventListener('click', () => {
+    value =0
+  number.textContent=value
+})
